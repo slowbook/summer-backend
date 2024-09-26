87c105d84cedacc361dcf790ca156d8f1f6c29d2
shashank <shashank200418@gmail.com>
2024-09-26 16:21:30 +0530

    cors

--- index.js
+++ index.js
@@ -7,18 +7,7 @@ const cors = require("cors") ;
 
 
 
-var allowlist = ['https://summer-frontend.vercel.app/']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-} else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
 
-app.use(cors(corsOptionsDelegate)) ;
 
 app.use(express.json()) ;
 
